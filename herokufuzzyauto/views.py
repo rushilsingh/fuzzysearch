2f2386b45ed90d19e8c3f7e03a6fb9d898fb5ea1
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 00:25:00 +0530

    Performance

--- herokufuzzyauto/views.py
+++ herokufuzzyauto/views.py
@@ -5,27 +5,10 @@ from django.shortcuts import render
 from fuzzy_search import DataSet
 import redis
 import os
-from itertools import izip_longest
 
 def index(request):
    
     red = redis.from_url(os.environ.get('REDIS_URL'), decode_responses=True)
-    #value = load_data_async("the")
     value = hget("h", "the")
 
     return render(request, 'base.html', {'data': value})
-
-
-from djutils.decorators import async
-
-@async
-def load_data_async(key):
-    for keybatch in batcher(red.scan_iter("*the*"), 500):
-        value = key
-        break
-    return value
-
-def batcher(iterable, n):
-    args = [iter(iterable)] * n
-    return izip_longest(*args)
-
