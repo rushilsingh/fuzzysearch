89c39edc222f6ed743fbb26183a5fc6a04365af8
Rushil Singh <rushilsingh@gmail.com>
2019-08-06 21:22:30 +0530

    Setting up backend logic

--- herokufuzzyauto/views.py
+++ herokufuzzyauto/views.py
@@ -3,13 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from fuzzy_search import DataSet
+import redis
+
 def index(request):
-    red = redis.from_url(os.environ.get('REDIS_URL'), decode_responses=True)
+   
+   red = redis.from_url(os.environ.get('REDIS_URL'), decode_responses=True)
     keys = red.keys("*")
     value = red.get(keys[0)]
 
 
-    return render(request, 'base.html', {'data':value})
+    return render(request, 'base.html', {'data': value})
 
 
 
