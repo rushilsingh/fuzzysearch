ca0cbcb5c51c1c4ba0195085ad403392196b5ff9
Rushil Singh <rushilsingh@gmail.com>
2019-08-06 23:21:22 +0530

    Fetching keys in batches

--- herokufuzzyauto/views.py
+++ herokufuzzyauto/views.py
@@ -5,18 +5,18 @@ from django.shortcuts import render
 from fuzzy_search import DataSet
 import redis
 import os
+from itertools import izip_longest
 
 def index(request):
    
     red = redis.from_url(os.environ.get('REDIS_URL'), decode_responses=True)
-    #keys = red.keys("23135851162")
-    #keys.sort().reverse()
-
-    for key in red.scan_iter("the"):
+    for keybatch in batcher(red.scan_iter("*the*"), 500):
         value = key
         break
-
     return render(request, 'base.html', {'data': value})
 
 
+def batcher(iterable, n):
+    args = [iter(iterable)] * n
+    return izip_longest(*args)
 
