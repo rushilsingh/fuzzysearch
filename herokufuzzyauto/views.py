fbacf2aa339c122624538b84dd83a27eefbb75c2
Rushil Singh <rushilsingh@gmail.com>
2019-08-06 20:38:47 +0530

    Fixed views.py

--- herokufuzzyauto/views.py
+++ herokufuzzyauto/views.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 def index(request):
-    return render(request, base.html, {'data':'dummy'})
+    return render(request, 'base.html', {'data':'dummy'})
 
