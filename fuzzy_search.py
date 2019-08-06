4e844e8f74e786c63b1afe6ebfbbc25b153bfd4b
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 00:22:12 +0530

    Performance

--- fuzzy_search.py
+++ fuzzy_search.py
@@ -21,7 +21,7 @@ class DataSet(object):
         n = 1
         for line in self.text:
             word, frequency = line.split()   
-            pipe.set(word, frequency)
+            pipe.hset("h", word, frequency)
             n = n + 1
             if (n % 64) == 0:
                 pipe.execute()
