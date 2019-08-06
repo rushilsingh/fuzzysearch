7234313a63685f4d55f509613d20ba0a117faba6
Rushil Singh <rushilsingh@gmail.com>
2019-08-06 20:10:24 +0530

    Setting up front-end

--- herokufuzzyauto/views.py
+++ herokufuzzyauto/views.py
@@ -1,8 +1,22 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+"""fuzzyauto URL Configuration
 
-from django.shortcuts import render
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+#from django.contrib import admin
+from . import views
 
-def index(request):
-    return render(request, base.html. {'data':'dummy'})
-
+urlpatterns = [
+    url(r'^.*$', views.index) 
+]
