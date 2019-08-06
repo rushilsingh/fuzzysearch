e9caffc240eda7d2c3a433ab8ca30b21a3749973
Rushil Singh <rushilsingh@gmail.com>
2019-08-06 23:28:14 +0530

    Performance

--- herokufuzzyauto/views.py
+++ herokufuzzyauto/views.py
@@ -10,11 +10,18 @@ from itertools import izip_longest
 def index(request):
    
     red = redis.from_url(os.environ.get('REDIS_URL'), decode_responses=True)
-    for keybatch in batcher(red.scan_iter("onyx"), 500):
-        value = key
-        break
+    value = load_data_async("the")
     return render(request, 'base.html', {'data': value})
 
+
+from djutils.decorators import async
+
+@async
+def load_data_async(key):
+    for keybatch in batcher(red.scan_iter("*the*"), 500):
+        value = key
+        break
+    return value
 
 def batcher(iterable, n):
     args = [iter(iterable)] * n
