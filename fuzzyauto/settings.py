7d6b874e802d5a5ce25694d280a919731359f8fe
Rushil Singh <rushilsingh@gmail.com>
2019-08-06 21:49:36 +0530

    Working on redis

--- fuzzyauto/settings.py
+++ fuzzyauto/settings.py
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'fuzzyauto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+import os
+CACHES = {
+    "default": {
+         "BACKEND": "redis_cache.RedisCache",
+         "LOCATION": os.environ.get('REDIS_URL'),
     }
 }
-
+ 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
