29b39c366536a1b32227b05220f74ff7bd4b7476
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 01:59:05 +0530

    Fixing url regex

--- fuzzyauto/urls.py
+++ fuzzyauto/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url
 #from django.contrib import admin
 from herokufuzzyauto import views
 urlpatterns = [
-     url(r'^/<word>/$', views.index)
+     url(r'^(?P<word>.*/', views.index)
 ]
