f1d3dd3620c0e19f06d6c019a464072a41614457
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 01:39:00 +0530

    debugging

--- fuzzyauto/urls.py
+++ fuzzyauto/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 #from django.contrib import admin
-import herokufuzzyauto views
+import herokufuzzyauto import views
 urlpatterns = [
      url(r'^/<word>/$', views.index),
 ]
