39a966c48758c10881ac98853684e76fe43f8aa6
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 01:04:42 +0530

    pipelining

--- herokufuzzyauto/views.py
+++ herokufuzzyauto/views.py
@@ -9,10 +9,18 @@ import os
 def index(request):
    
     red = redis.from_url(os.environ.get('REDIS_URL'), decode_responses=True)
-    values = []
-    for key in red.scan_iter("*procra*"):
-        value = key
-        values.append
+    values = find("*pro*")
     values = str(values)
-
     return render(request, 'base.html', {'data': values})
+
+def find(key):
+    values = []
+    pipe = red.pipeline()
+    n = 1
+    for key in red.scan_iter("*procra*"):
+        values.append(key)
+        n = n+ 1
+        if (n % 64) == 0:
+            pipe.execute()
+            pipe = red.pipeline()
+    return values
