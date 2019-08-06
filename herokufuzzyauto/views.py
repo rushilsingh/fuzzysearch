f61d859196fe13e08e7252c8ffdcd52701b259ee
Rushil Singh <rushilsingh@gmail.com>
2019-08-06 21:09:46 +0530

    Setting up redis database

--- herokufuzzyauto/views.py
+++ herokufuzzyauto/views.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from fuzzy_search import DataSet
+d = DataSet()
 def index(request):
     return render(request, 'base.html', {'data':'dummy'})
 
