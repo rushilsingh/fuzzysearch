27499951d11c536bd1190865cd8632cff641d11c
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 23:32:51 +0530

    Using RedirectView for favicon rendering

--- fuzzyauto/urls.py
+++ fuzzyauto/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from herokufuzzyauto import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
      url(r'^search/{0,1}$', views.endpoint),
-     url(r'^/{0,1}$', views.index)
+     url(r'^/{0,1}$', views.index),
+     url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 
 ]
