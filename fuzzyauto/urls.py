0691413f38cbd433738988ca12adc4190fc4af20
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 21:52:56 +0530

    Building UI skeleton

--- fuzzyauto/urls.py
+++ fuzzyauto/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import url
 from herokufuzzyauto import views
 urlpatterns = [
      url(r'^search/{0,1}$', views.endpoint),
-     url(r'ui/{0,1}$', views.index)
+     url(r'', views.index)
 
 ]
