dc2d2716ab82c9280f7ec041cb3579b296c86dc1
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 23:44:12 +0530

    Pruning files

--- fuzzyauto/urls.py
+++ fuzzyauto/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import url
 from herokufuzzyauto import views
-from django.views.generic import RedirectView
 
 urlpatterns = [
      url(r'^search/{0,1}$', views.endpoint),
-     url(r'^/{0,1}$', views.index),
-     url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
-
+     url(r'^/{0,1}$', views.index)
 ]
