05d62ffaf8b747eb9e7965dff5d6748c224bd5a8
Rushil Singh <rushilsingh@gmail.com>
2019-08-07 18:43:08 +0530

    Moved processing out of views.py

--- fuzzy_search.py
+++ fuzzy_search.py
@@ -26,6 +26,56 @@ class DataSet(object):
             if (n % 64) == 0:
                 pipe.execute()
                 pipe = red.pipeline()
+    def find(self, key):
+
+    values = []
+    red = redis.from_url(os.environ.get('REDIS_URL'), decode_responses=True)
+    #red = redis.Redis()
+    pipe = red.pipeline()
+    search = "*" + key + "*"
+    cursor = 0
+    if red.hexists("h", key):
+        while True:
+            cursor, value = red.hscan("h", cursor, search, 1000)
+            if cursor == 0:
+                break
+            if value != {}:
+                values.append(value)
+    else:
+        values = {}
+    mapping = values 
+    values = []
+    current = 1
+    for dic in mapping:
+        for entry in dic:
+            values.append(str(entry)[2:])
+            current += 1
+    
+    if not values:
+        return {}
+
+    if len(values) == 1:
+        return {1:values[0]}
+
+    results = {}
+    unsorted = []
+    exact = None
+    start = []
+    for value in values:
+        if value == key:
+            exact = value
+        elif value.startswith(key) and value != key:
+            start.append(value)
+        else:   
+            unsorted.append(value)
+
+    results["exact"] = exact
+    results["start"] = start 
+    results["unsorted"] = unsorted
+    current = 1
+    sorted_results = {}
+    if results["exact"]:
+        sorted_results[current] = results["exact"]
 
 if __name__ == "__main__":
     d = DataSet()
