53bb2a91346d7457d4c6d7975710a4897bffa253
Rushil Singh <rushilsingh@gmail.com>
2019-08-08 18:18:27 +0530

    Trying to fix staticfiles issue

--- fuzzyauto/settings.py
+++ fuzzyauto/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = None
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, "static")
