4a57f299186b99ed3893ad48711aa3fbb77a260c
Rushil Singh <rushilsingh@gmail.com>
2019-08-10 00:50:31 +0530

    Attempting favicon via staticfiles

--- fuzzyauto/settings.py
+++ fuzzyauto/settings.py
@@ -123,5 +123,5 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "static")
+    os.path.join(PROJECT_DIR, "media")
 ]
