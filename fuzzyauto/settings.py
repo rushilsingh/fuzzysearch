6d437fd02840c52e00f5ec9981aa5c440fe9a7b4
Rushil Singh <rushilsingh@gmail.com>
2019-08-14 22:44:50 +0530

    Fixing settings.py

--- fuzzyauto/settings.py
+++ fuzzyauto/settings.py
@@ -122,8 +122,3 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT =    os.path.join(PROJECT_DIR, "staticfiles")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "media")
-]
-
-
